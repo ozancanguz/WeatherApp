521da85728c887864207e4b29b9f39d684a394da
Ozan <ozancangu24@gmail.com>
2023-02-03 16:17:21 +0300

    -project structure constructed

--- app/src/main/java/com/ozancanguz/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/weatherapp/ui/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.ozancanguz.weatherapp
+package com.ozancanguz.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ozancanguz.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     }
 }
